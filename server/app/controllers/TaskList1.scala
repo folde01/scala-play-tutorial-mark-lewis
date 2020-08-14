5debb754bfcc109b9dcac5f26bad3a3708372eb6
Jesse Oldershaw <jesse.oldershaw@rpharms.com>
2020-08-14 18:07:53 +0100

    video 8 form data with post 2

--- server/app/controllers/TaskList1.scala
+++ server/app/controllers/TaskList1.scala
@@ -19,8 +19,8 @@ class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(c
     postVals.map { args =>
       val username = args("username").head
       val password = args("password").head
-      Ok(s"$username logged in with $password")
-    }.getOrElse(Ok("Ooops"))
+      Redirect(routes.TaskList1.taskList()) // reverse routing
+    }.getOrElse(Redirect(routes.TaskList1.login()))
   }
 
   def taskList = Action {
