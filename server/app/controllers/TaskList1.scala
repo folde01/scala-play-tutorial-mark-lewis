1e493d782493fc9ecf02ddb390ce08ff5813f275
Jesse Oldershaw <jesse.oldershaw@rpharms.com>
2020-08-14 16:28:18 +0100

    video 6 form data with get

--- server/app/controllers/TaskList1.scala
+++ server/app/controllers/TaskList1.scala
@@ -5,6 +5,15 @@ import play.api.mvc.{AbstractController, ControllerComponents}
 
 @Singleton
 class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(cc)  {
+
+  def login() = Action {
+    Ok(views.html.login1())
+  }
+
+  def validateLoginGet(username: String, password: String) = Action {
+    Ok(s"$username logged in with $password")
+  }
+
   def taskList = Action {
     val tasks = List("task1", "task2", "tasks3", "sleep", "eat")
     Ok(views.html.tasklist1(tasks))
@@ -13,4 +22,5 @@ class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(c
   def product(prodName: String, prodNum: Int) = Action {
     Ok(s"name: $prodName, num: $prodNum")
   }
+
 }
