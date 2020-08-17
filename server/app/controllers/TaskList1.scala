870c580301759f19e3b08f278a8f1ab0ca3132c7
Jesse Oldershaw <jesse.oldershaw@rpharms.com>
2020-08-17 13:00:00 +0100

    video 13 flash scope

--- server/app/controllers/TaskList1.scala
+++ server/app/controllers/TaskList1.scala
@@ -15,29 +15,29 @@ class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(c
     Ok(s"$username logged in with $password")
   }
 
-  def validateLoginPost = Action { request =>
+  def validateLoginPost = Action { implicit request =>
     val postVals = request.body.asFormUrlEncoded
     postVals.map { args =>
       val username = args("username").head
       val password = args("password").head
       if (TaskListInMemoryModel.validateUser(username, password))
         Redirect(routes.TaskList1.taskList()).withSession("username" -> username) // reverse routing
-      else Redirect(routes.TaskList1.login())
+      else Redirect(routes.TaskList1.login()).flashing("error" -> "Invalid username/password combination")
     }.getOrElse(Redirect(routes.TaskList1.login()))
   }
 
-  def createUser = Action { request =>
+  def createUser = Action { implicit request =>
     val postVals = request.body.asFormUrlEncoded
     postVals.map { args =>
       val username = args("username").head
       val password = args("password").head
       if (TaskListInMemoryModel.createUser(username, password))
         Redirect(routes.TaskList1.taskList()).withSession("username" -> username) // reverse routing
-      else Redirect(routes.TaskList1.login())
+      else Redirect(routes.TaskList1.login()).flashing("error" -> "user creation failed")
     }.getOrElse(Redirect(routes.TaskList1.login()))
   }
 
-  def taskList = Action { request =>
+  def taskList = Action { implicit request =>
     val usernameOption = request.session.get("username")
     usernameOption.map { username =>
       val tasks = TaskListInMemoryModel.getTasks(username)
